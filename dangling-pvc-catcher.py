6feafc8db7e80b4e12767507c556302ba9e764a5
Alexandre Perlmutter <alexandre.perlmutter@gmail.com>
2023-12-08 15:03:21 +0100

    Improve code

--- dangling-pvc-catcher.py
+++ dangling-pvc-catcher.py
@@ -19,39 +19,41 @@ def get_replicas_from(resource) -> int:
     except AttributeError:
         return int(resource.status.number_available) if resource.status.number_available else 0
     
-def get_pvcs_from(resources: list) -> set:
-    running_resources = filter(lambda resource: get_replicas_from(resource) > 0, resources)
-    resources_with_pvc = filter(lambda resource: resource.spec.template.spec.volumes and resource.spec.template.spec.volumes[0].persistent_volume_claim, running_resources)
+def get_unused_pvcs_from(resources: list) -> set[tuple[str, str]]:
+    stopped_resources = filter(lambda resource: get_replicas_from(resource) == 0, resources)
+    resources_with_unused_pvcs = list(filter(lambda resource: resource.spec.template.spec.volumes and resource.spec.template.spec.volumes[0].persistent_volume_claim, stopped_resources))
     pvcs = set()
-    for resource in resources_with_pvc:
+    for resource in resources_with_unused_pvcs:
         for pvc in map(lambda volume: volume.persistent_volume_claim, filter(lambda volume: volume.persistent_volume_claim, resource.spec.template.spec.volumes)):
-            pvcs.add(format_resource(resource.metadata.namespace, pvc.claim_name))
+            pvcs.add((format_resource(resource.metadata.namespace, pvc.claim_name), format_resource(resource.metadata.namespace, resource.metadata.name)))
     return pvcs
 
-def get_pvcs_from_deploymentconfigs() -> set:
+def get_unused_pvcs_from_deploymentconfigs() -> set[tuple[str, str]]:
     result = set()
     for resource in CustomObjectsApi().list_cluster_custom_object("apps.openshift.io", "v1", "deploymentconfigs").get("items", []):
         for volume in resource.get("spec").get("template").get("spec").get("volumes", []):
-            if volume.get("persistentVolumeClaim", None) and resource.get("status").get("availableReplicas", 0) > 0:
-                result.add(format_resource(resource.get("metadata").get("namespace"), volume.get("persistentVolumeClaim").get("claimName")))
-    print(result)
+            if volume.get("persistentVolumeClaim", None) and not resource.get("status").get("availableReplicas"):
+                result.add((format_resource(resource.get("metadata").get("namespace"), volume.get("persistentVolumeClaim").get("claimName")), format_resource(resource.get("metadata").get("namespace"), resource.get("metadata").get("name"))))
     return result
 
-def get_pvcs_used_by_resources() -> set:
+def get_pvcs_unused_by_resources() -> set:
     result = set()
-    result |= get_pvcs_from(api_apps.list_deployment_for_all_namespaces().items)
-    result |= get_pvcs_from(api_apps.list_stateful_set_for_all_namespaces().items)
-    result |= get_pvcs_from(api_apps.list_daemon_set_for_all_namespaces().items)
-    result |= get_pvcs_from(api_apps.list_replica_set_for_all_namespaces().items)
-    result |= get_pvcs_from_deploymentconfigs()
+    result |= get_unused_pvcs_from(api_apps.list_deployment_for_all_namespaces().items)
+    result |= get_unused_pvcs_from(api_apps.list_stateful_set_for_all_namespaces().items)
+    result |= get_unused_pvcs_from(api_apps.list_daemon_set_for_all_namespaces().items)
+    result |= get_unused_pvcs_from(api_apps.list_replica_set_for_all_namespaces().items)
+    result |= get_unused_pvcs_from_deploymentconfigs()
     return result
 
-all_pvcs = get_all_pvcs()
-pvcs_used_by_resources = get_pvcs_used_by_resources()
-dangling_pvcs = all_pvcs - pvcs_used_by_resources
+dangling_pvcs = get_pvcs_unused_by_resources()
+dangling_pvcs_without_namespace = set(map(lambda pvc: pvc[0], dangling_pvcs))
+dangling_pvcs_count = len(dangling_pvcs_without_namespace)
+pvc_count = len(get_all_pvcs())
 
-print(f"All PVCs count: {len(all_pvcs)}")
-print(f"PVCs used by resources count: {len(pvcs_used_by_resources)}")
-print(f"Dangling PVCs count: {len(dangling_pvcs)}")
-print("\nDANGLING PVCs:\n")
-print("\n".join(sorted(list(all_pvcs - pvcs_used_by_resources))))
+print(f"All PVCs count: {pvc_count}")
+print(f"PVCs used by resources count: {pvc_count - dangling_pvcs_count}")
+print(f"Dangling PVCs count: {dangling_pvcs_count}")
+print("\nDANGLING PVCs (without NS):\n")
+print("\n".join(sorted(list(dangling_pvcs_without_namespace))))
+print("\nDANGLING PVCs (with NS):\n")
+print("\n".join(list(f"{pvc[0]}\tFROM: {pvc[1]}" for pvc in sorted(list(dangling_pvcs), key=lambda pvc: pvc[0]))))
